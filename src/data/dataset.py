e635517e41df66a2a9e169748151cbff9c90c468
sevlll <82900814+sevlll@users.noreply.github.com>
2025-04-24 19:19:26 +0300

    Update dataset.py

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -2,14 +2,14 @@ import torchaudio
 from torch.utils.data import Dataset
 import os
 
-
 class LibriSpeechDataset(Dataset):
     def __init__(self, data_dir, url, download=True):
+        folder = os.path.join(data_dir, 'LibriSpeech', url)
+        dl = download and not os.path.isdir(folder)
         self.dataset = torchaudio.datasets.LIBRISPEECH(
             root=data_dir,
             url=url,
-            download=download,
-            folder_in_archive=os.path.join(data_dir, "LibriSpeech")
+            download=dl
         )
 
     def __getitem__(self, index):
