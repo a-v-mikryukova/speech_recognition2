5523e6b9b16be28393dbb6c5dc5aada44deea595
Микрюкова Алёна <156831971+a-v-mikryukova@users.noreply.github.com>
2025-05-06 18:10:43 +0300

    Update preprocess.py

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -6,33 +6,62 @@ from torch import nn
 class TextTransform:
     """Maps characters to integers and vice versa."""
 
-    def __init__(self) -> None:
+    def __init__(self):
+        char_map_str = """
+        ' 0
+         1
+        a 2
+        b 3
+        c 4
+        d 5
+        e 6
+        f 7
+        g 8
+        h 9
+        i 10
+        j 11
+        k 12
+        l 13
+        m 14
+        n 15
+        o 16
+        p 17
+        q 18
+        r 19
+        s 20
+        t 21
+        u 22
+        v 23
+        w 24
+        x 25
+        y 26
+        z 27
+        """
         self.char_map = {}
         self.index_map = {}
-        for ch in "' abcdefghijklmnopqrstuvwxyz":
-            index = ord(ch) - ord("a") + 2
-            if ch == "'":
-                index = 0
-            if ch == " ":
-                index = 1
+        for line in char_map_str.strip().split('\n'):
+            ch, index = line.split()
             self.char_map[ch] = int(index)
             self.index_map[int(index)] = ch
-        self.index_map[1] = " "
+        self.index_map[1] = ' '
 
     def text_to_int(self, text):
-        """Use a character map and convert text to an integer sequence."""
+        """ Use a character map and convert text to an integer sequence """
         int_sequence = []
         for c in text:
-            ch = self.char_map[c]
+            if c == ' ':
+                ch = self.char_map['']
+            else:
+                ch = self.char_map[c]
             int_sequence.append(ch)
         return int_sequence
 
     def int_to_text(self, labels):
-        """Use a character map and convert integer labels to an text sequence."""
+        """ Use a character map and convert integer labels to an text sequence """
         string = []
         for i in labels:
             string.append(self.index_map[i])
-        return "".join(string).replace("", " ")
+        return ''.join(string).replace('', ' ')
 
 
 def get_featurizer(sample_rate, n_mels, train=True):
@@ -57,25 +86,13 @@ def collate_fn(batch, text_transform, feature_type="train", sample_rate=16000, n
         spec = audio_transform(waveform).squeeze(0).transpose(0, 1)
         spectrograms.append(spec)
 
-        label = torch.tensor(text_transform.text_to_int(utterance.lower()), dtype=torch.long)
+        label = torch.Tensor(text_transform.text_to_int(utterance.lower()))
         labels.append(label)
 
         input_lengths.append(spec.shape[0] // 2)
         label_lengths.append(len(label))
 
-    spectrograms = nn.utils.rnn.pad_sequence(
-        spectrograms,
-        batch_first=True,
-    ).unsqueeze(1).transpose(2, 3)
+    spectrograms = nn.utils.rnn.pad_sequence(spectrograms, batch_first=True).unsqueeze(1).transpose(2, 3)
+    labels = nn.utils.rnn.pad_sequence(labels,batch_first=True)
 
-    labels = nn.utils.rnn.pad_sequence(
-        labels,
-        batch_first=True,
-    )
-
-    return (
-        spectrograms,
-        labels,
-        torch.tensor(input_lengths, dtype=torch.long),
-        torch.tensor(label_lengths, dtype=torch.long),
-    )
+    return spectrograms, labels, input_lengths, label_lengths
