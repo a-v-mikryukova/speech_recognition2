ff4fed123d5937ebad13820e533271267d042d0e
Микрюкова Алёна <156831971+a-v-mikryukova@users.noreply.github.com>
2025-05-06 19:49:31 +0300

    Update preprocess.py

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -4,43 +4,16 @@ from torch import nn
 
 
 class TextTransform:
-    """Maps characters to integers and vice versa."""
-
+    """Maps characters to integers and vice versa"""
     def __init__(self):
-        char_map_str = """
-        ' 0
-         1
-        a 2
-        b 3
-        c 4
-        d 5
-        e 6
-        f 7
-        g 8
-        h 9
-        i 10
-        j 11
-        k 12
-        l 13
-        m 14
-        n 15
-        o 16
-        p 17
-        q 18
-        r 19
-        s 20
-        t 21
-        u 22
-        v 23
-        w 24
-        x 25
-        y 26
-        z 27
-        """
         self.char_map = {}
         self.index_map = {}
-        for line in char_map_str.strip().split('\n'):
-            ch, index = line.split()
+        for ch in "' abcdefghijklmnopqrstuvwxyz":
+            index = ord(ch) - ord('a') + 2
+            if ch == "'":
+                index = 0
+            if ch == " ":
+                index = 1
             self.char_map[ch] = int(index)
             self.index_map[int(index)] = ch
         self.index_map[1] = ' '
@@ -49,10 +22,7 @@ class TextTransform:
         """ Use a character map and convert text to an integer sequence """
         int_sequence = []
         for c in text:
-            if c == ' ':
-                ch = self.char_map['']
-            else:
-                ch = self.char_map[c]
+            ch = self.char_map[c]
             int_sequence.append(ch)
         return int_sequence
 
@@ -62,6 +32,8 @@ class TextTransform:
         for i in labels:
             string.append(self.index_map[i])
         return ''.join(string).replace('', ' ')
+  
+
 
 
 def get_featurizer(sample_rate, n_mels, train=True):
