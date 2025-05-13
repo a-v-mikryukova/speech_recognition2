3825af81ecd29cbfe994ae58dced48613bf266e2
Микрюкова Алёна <156831971+a-v-mikryukova@users.noreply.github.com>
2025-05-13 12:48:50 +0300

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,13 @@
 from .logger import WanDBLogger
 from .metrics import avg_wer, cer, wer
+from .inference import quantize_model, inference_speed, global_pruning
 
 __all__ = [
     "WanDBLogger",
     "avg_wer",
     "cer",
     "wer",
+    "quantize_model",
+    "inference_speed",
+    "global_pruning"
 ]
